f0537e4b883fdb54cde1f4c43147bfc2d391e669
Quazi Zarin Subah <155545263+Subah-Zarin@users.noreply.github.com>
2025-08-17 01:42:45 +0600

    connected dashboard to database

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\LeaveController;
@@ -10,9 +11,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.dashboard'); 
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard')
+    ->middleware('auth');
+
 
 Route::resource('attendance', AttendanceController::class);
 
